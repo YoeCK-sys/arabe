07d97366fe96cffb5a6d5daec3125592076e9eae
Tu Nombre <darkyoel75@gmail.com>
2024-05-31 21:01:11 -0300

    Inicialización del proyecto

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Empire from '@/app/components/home';
+
+export default function Home() {
+  return (
+    <div>
+      <Empire/>
+    </div>
+  );
+}
