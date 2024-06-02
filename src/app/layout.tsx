282f2e086710e67af490facae791d0aaac8487fe
Tu Nombre <darkyoel75@gmail.com>
2024-06-02 20:02:22 -0300

    UPDATE 0.92

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ArabeCS Downloads",
   description: "CS1.6 DOWNLOAD CHEATS",
+  openGraph: {
+    title: "ArabeCS Downloads",
+    description: "CS1.6 DOWNLOAD CHEATS",
+    images: [
+      {
+        url: "/logoback.png",
+        width: 800,
+        height: 600,
+        alt: "ArabeCS Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="ArabeCS Downloads" />
+        <meta property="og:description" content="CS1.6 DOWNLOAD CHEATS" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
