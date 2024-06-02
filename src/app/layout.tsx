02d277d85d6205c1effd790be88063643e757d95
Tu Nombre <darkyoel75@gmail.com>
2024-06-02 20:22:26 -0300

    UPDATE 0.92

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,17 @@ export const metadata: Metadata = {
       },
     ],
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "ArabeCS Downloads",
+    description: "CS1.6 DOWNLOAD CHEATS",
+    images: [
+      {
+        url: "/logo.png",
+        alt: "ArabeCS Logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -29,11 +40,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Meta tags for Open Graph */}
         <meta property="og:title" content="ArabeCS Downloads" />
         <meta property="og:description" content="CS1.6 DOWNLOAD CHEATS" />
         <meta property="og:image" content="/logo.png" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
+
+        {/* Meta tags for Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="ArabeCS Downloads" />
+        <meta name="twitter:description" content="CS1.6 DOWNLOAD CHEATS" />
+        <meta name="twitter:image" content="/logo.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
